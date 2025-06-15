8e0fda70723bbc555938d6ba2b96ea81226e0e72
Dan Jones <hello@danjones.dev>
2025-06-15 17:30:01 +0100

    Clean-up: remove old regex approach

--- src/atlassian_markup_transpiler.rs
+++ src/atlassian_markup_transpiler.rs
@@ -73,59 +73,6 @@ impl<'a> MarkUpNode<'a> {
     }
 }
 
-fn regex_approach(atlassian_markup: &str) -> String {
-    let num_tokens_estimate = (atlassian_markup.lines().count() as f32 * 1.5).ceil() as usize;
-    let mut tokenised_string: Vec<MarkUpNode> = Vec::with_capacity(num_tokens_estimate);
-
-    let markup_regex =
-        RegexBuilder::new(r"(?<heading>^h(?<heading_level>[1-6])\..*$)|(?<code_block>^\{code(?<code_lang>:.+)\}(?<code_content>.*))|(?<plain_text>^.+$)")
-            .multi_line(true)
-            .build()
-            .unwrap();
-
-    for marked_content in markup_regex.captures_iter(atlassian_markup) {
-        if let Some(heading) = marked_content.name("heading") {
-            let heading_level = marked_content.name("heading_level").unwrap();
-            let content = &heading.as_str()[4..];
-
-            if heading_level.as_str() == "1" {
-                tokenised_string.push(MarkUpNode::Heading1(content));
-            } else if heading_level.as_str() == "2" {
-                tokenised_string.push(MarkUpNode::Heading2(content));
-            } else if heading_level.as_str() == "3" {
-                tokenised_string.push(MarkUpNode::Heading3(content));
-            } else if heading_level.as_str() == "4" {
-                tokenised_string.push(MarkUpNode::Heading4(content));
-            } else if heading_level.as_str() == "5" {
-                tokenised_string.push(MarkUpNode::Heading5(content));
-            } else if heading_level.as_str() == "6" {
-                tokenised_string.push(MarkUpNode::Heading6(content));
-            }
-        } else if let Some(_) = marked_content.name("code_block") {
-            let code_block_language = marked_content.name("code_lang").map(|x| x.as_str());
-            // tokenised_string.push(MarkUpNode::CodeBlock(code_block_language));
-            // if let Some(code_block_content) = marked_content.name("code_content") {
-            //     if code_block_content.len() > 0 {
-            //         tokenised_string.push(MarkUpNode::PlainText(code_block_content.as_str()));
-            //     }
-            // }
-        } else if let Some(text) = marked_content.name("plain_text") {
-            tokenised_string.push(MarkUpNode::PlainText(text.as_str()));
-        } else {
-            panic!("Marlformed marked content regex caused a sub-string to not match");
-        }
-    }
-
-    let mut result = tokenised_string.iter().fold(
-        String::with_capacity(atlassian_markup.len() * 2),
-        |mut acc, x| {
-            x.push_content_onto_string(&mut acc);
-            acc
-        },
-    );
-    result.shrink_to_fit();
-    result
-}
 
 fn heading_ast_node_from_count<'a>(count: u32) -> impl Fn(&'a str) -> MarkUpNode<'a> {
     match count {
